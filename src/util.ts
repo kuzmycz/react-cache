8376bfb7ef3e6c9a5ec42e40339180c7afc66efe
Mark Kuzmycz <kuzmycz@gmail.com>
2020-03-26 07:14:05 +1100

    Code refinement

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@ export const deepCopy = (obj: any): any => {
   if (Array.isArray(obj)) {
     return [...obj];
   } else if (typeof obj === 'object') {
-    let copy: any = {};
+    const copy: any = {};
     Object.keys(obj).forEach(key => (copy[key] = deepCopy(obj[key])));
     return copy;
   } else {
