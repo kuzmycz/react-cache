9a577d05434b54b697ef190dc5d9d5995f40a6fd
kuzmycz@gmail.com <YNNFptoB3KDNorsAHM>
2021-06-01 06:19:02 +1000

    Added a named cache

--- src/cache.tsx
+++ src/cache.tsx
@@ -6,20 +6,33 @@ export type CacheProps = {
   values: any;
   name?: string;
   observers?: CacheObserver[];
+  namedStore?: string;
   children: React.ReactNode;
 };
 
-export const Cache = ({ values, observers = [], children }: CacheProps) => {
+export const Cache = ({ values, namedStore, observers = [], children }: CacheProps) => {
 
   const ref = useRef<CacheBag>();
   console.log("Cache Creation: ", ref, ref && ref.current);
 
   if (ref.current !== undefined || ref.current !== null) {
-    ref.current = new CacheBag(values, observers);
+    if (!!namedStore) {
+      let global: any = window;
+      if (!global[namedStore]) {
+        global[namedStore] = {
+          current: new CacheBag(values, observers),
+        }
+      }
+      console.log("Cache Creation: Using external ref", namedStore);
+      ref.current = global[namedStore]?.current;
+
+    } else {
+      ref.current = new CacheBag(values, observers);
+    }
     console.log("Cache Creation: Bag created", ref && ref.current);
   }
 
-  return <CacheContext.Provider value={ref.current}>{children}</CacheContext.Provider>;
+  return <CacheContext.Provider value={ref.current ?? new CacheBag(values, observers)}>{children}</CacheContext.Provider>;
 };
 
 export const CacheContext = React.createContext(new CacheBag({name: 'Default Cache'}));
